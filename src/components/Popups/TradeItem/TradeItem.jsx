d895a9c6c409bc632114dba8fafbb741cb90c2f2
Ardonit Saliji <ardonit.1980@gmail.com>
2022-08-02 02:43:08 +0200

    adding gem size and form

--- src/components/Popups/TradeItem/TradeItem.jsx
+++ src/components/Popups/TradeItem/TradeItem.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useRef, useState } from 'react'
 import './TradeItem.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, A11y } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 const TradeItem = () => {
   const [spanValue, setSpanValue] = useState(1)
-  const [optionState, setOptionState] = useState(true)
+  const [optionState, setOptionState] = useState(false)
   const inputRef = useRef()
   const spanRef = useRef()
   const inputChangedHandler = () => {
@@ -15,25 +21,63 @@ const TradeItem = () => {
   const hideInput = () => {
     spanRef.current.classList.remove('show')
   }
-  const switchOption = () => {
-    setOptionState((prev) => !prev)
-  }
+
   return (
     <div className='trade-container'>
       <div className='trade'>
         <div className='trade-options'>
-          <a onClick={switchOption} className={optionState && 'active'}>
+          <a
+            style={!optionState ? { width: '90%', backgroundColor: 'green' } : { width: '7rem' }}
+            onClick={() => setOptionState((prev) => !prev)}
+          >
             Buy
           </a>
-          <a onClick={switchOption} className={!optionState && 'active'}>
+          <a
+            style={optionState ? { width: '90%', backgroundColor: 'green' } : { width: '7rem' }}
+            onClick={() => setOptionState((prev) => !prev)}
+          >
             Sell
           </a>
         </div>
         <div className='trade-image'>
           <h2>Citrine</h2>
-          <img src={require('../../../images/citrine.png')} alt='' />
+          <Swiper
+            modules={[Navigation, Pagination, A11y]}
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+          >
+            <SwiperSlide>
+              <img src={require('../../../images/citrine.png')} alt='' />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={require('../../../images/cit1.jpg')} alt='' />
+            </SwiperSlide>
+          </Swiper>
         </div>
         <div className='trade-quantity'>
+          <div className='trade-form'>
+            <ul>
+              <li>Heart</li>
+              <li>Oval</li>
+              <li>Round</li>
+              <li>Octagon</li>
+              <li>Trillion</li>
+              <li>Pear</li>
+            </ul>
+          </div>
+          <div className='trade-size'>
+            <ul>
+              <li>4 - 6 mm</li>
+              <li>6 - 8 mm</li>
+              <li>8 - 10 mm</li>
+              <li>10 - 12 mm</li>
+              <li>12 - 14 mm</li>
+              <li>14 - 20 mm</li>
+            </ul>
+          </div>
+
           <div className='trade-slider'>
             <div className='trade-slider-value'>
               <span ref={spanRef}>{spanValue}</span>
@@ -53,8 +97,6 @@ const TradeItem = () => {
             <div className='trade-val right'>10</div>
           </div>
         </div>
-
-        {/* <AiOutlineClose className='x' /> */}
       </div>
     </div>
   )
