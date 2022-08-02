bbac9202740edbf8d880a32d97c4038ca7c8e7c1
Ardonit Saliji <ardonit.1980@gmail.com>
2022-08-02 10:35:18 +0200

    changed trade and refactored it

--- src/components/Popups/TradeItem/TradeItem.jsx
+++ src/components/Popups/TradeItem/TradeItem.jsx
@@ -1,44 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useRef, useState } from 'react'
-import './TradeItem.css'
+import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, A11y } from 'swiper'
+import './TradeItem.css'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
-
+import { trade } from './trade'
 const TradeItem = () => {
-  const [spanValue, setSpanValue] = useState(1)
-  const [optionState, setOptionState] = useState(false)
-  const inputRef = useRef()
-  const spanRef = useRef()
-  const inputChangedHandler = () => {
-    spanRef.current.classList.add('show')
-    setSpanValue(inputRef.current.value)
-    spanRef.current.style.left = 5 + spanValue * 8 + '%'
-  }
-  const hideInput = () => {
-    spanRef.current.classList.remove('show')
-  }
-
   return (
     <div className='trade-container'>
       <div className='trade'>
-        <div className='trade-options'>
-          <a
-            style={!optionState ? { width: '90%', backgroundColor: 'green' } : { width: '7rem' }}
-            onClick={() => setOptionState((prev) => !prev)}
-          >
-            Buy
-          </a>
-          <a
-            style={optionState ? { width: '90%', backgroundColor: 'green' } : { width: '7rem' }}
-            onClick={() => setOptionState((prev) => !prev)}
-          >
-            Sell
-          </a>
-        </div>
         <div className='trade-image'>
           <h2>Citrine</h2>
           <Swiper
@@ -46,56 +19,46 @@ const TradeItem = () => {
             spaceBetween={50}
             slidesPerView={1}
             navigation
-            pagination={{ clickable: true }}
           >
-            <SwiperSlide>
-              <img src={require('../../../images/citrine.png')} alt='' />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={require('../../../images/cit1.jpg')} alt='' />
-            </SwiperSlide>
+            {trade.map(({ image, size, price }) => (
+              <SwiperSlide>
+                <img src={image} alt='' />
+                <div className='trade-price'>
+                  <h2>{size}</h2>
+                  <h2>{price}</h2>
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
-        </div>
-        <div className='trade-quantity'>
-          <div className='trade-form'>
-            <ul>
-              <li>Heart</li>
-              <li>Oval</li>
-              <li>Round</li>
-              <li>Octagon</li>
-              <li>Trillion</li>
-              <li>Pear</li>
-            </ul>
-          </div>
-          <div className='trade-size'>
-            <ul>
-              <li>4 - 6 mm</li>
-              <li>6 - 8 mm</li>
-              <li>8 - 10 mm</li>
-              <li>10 - 12 mm</li>
-              <li>12 - 14 mm</li>
-              <li>14 - 20 mm</li>
-            </ul>
-          </div>
 
-          <div className='trade-slider'>
-            <div className='trade-slider-value'>
-              <span ref={spanRef}>{spanValue}</span>
-            </div>
-            <div className='trade-val left'>1</div>
-            <input
-              className='trade-slide'
-              ref={inputRef}
-              type='range'
-              onChange={(e) => inputChangedHandler(e)}
-              onBlur={hideInput}
-              defaultValue='1'
-              min='1'
-              max='10'
-              step='0.1'
-            />
-            <div className='trade-val right'>10</div>
-          </div>
+          <hr className='the-SEPARATOR' />
+        </div>
+
+        <div className='trade-shape'>
+          <ul>
+            <li>Oval</li>
+            <li>Heart</li>
+            <li>Round</li>
+            <li>Octagon</li>
+            <li>Trillion</li>
+            <li>Pear</li>
+          </ul>
+        </div>
+
+        <div className='trade-size'>
+          <ul>
+            <li>4 - 6 mm</li>
+            <li>6 - 8 mm</li>
+            <li>8 - 10 mm</li>
+            <li>10 - 12 mm</li>
+            <li>12 - 15 mm</li>
+            <li>15 - 20 mm</li>
+          </ul>
+        </div>
+
+        <div className='trade-cart-btns'>
+          <button>Add to Cart</button>
+          <button>Cancel</button>
         </div>
       </div>
     </div>
---
+++ src/components/Popups/TradeItem/trade.js
@@ -0,0 +1,14 @@
+export const trade = [
+  {
+    image: require('./trade-images/citrine.png'),
+    size: '6.2 x 3.9 mm',
+    price: '$4.62',
+    shape: 'Oval',
+  },
+  {
+    image: require('./trade-images/citrine1.jpg'),
+    size: '5.1 x 4.9 mm',
+    price: '$7.62',
+    shape: 'Round',
+  },
+]
