e6ee74743bfa1f7a0375cdf1ecfe800eec7908e7
Ardonit Saliji <ardonit.1980@gmail.com>
2022-08-14 15:26:41 +0200

    fixed basket  item adding and removing

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,41 +1,35 @@
-import { useRef } from "react";
-import { useEffect } from "react";
-import "./Basket.css";
+import { useRef } from 'react'
+import { useEffect } from 'react'
+import './Basket.css'
 
 const Basket = ({ basketState, setBasketState }) => {
   //
-  const basket = JSON.parse(localStorage.getItem("basket"));
+  const basket = JSON.parse(localStorage.getItem('basket'))
   useEffect(() => {
-    const retriveProducts = JSON.parse(localStorage.getItem("basket"));
-    setBasketState(retriveProducts);
-  }, []);
-
-  const checkoutItemRef = useRef();
+    const retriveProducts = JSON.parse(localStorage.getItem('basket'))
+    setBasketState(retriveProducts)
+  }, [])
+  const checkoutItemRef = useRef()
   return (
-    <div className="basket-container">
-      <div className="basket">
+    <div className='basket-container'>
+      <div className='basket'>
         <h2>Basket Items</h2>
-        <div className="basket-items">
+        <div className='basket-items'>
           {basketState?.map(
             (el, i) =>
-              el?.image !==
-                require("../Popups/TradeItem/trade-images/sold.jpg") && (
-                <div key={i} className="basket-item">
+              el?.image !== require('../Popups/TradeItem/trade-images/sold.jpg') && (
+                <div key={i} className='basket-item'>
                   <h3>{el?.title}</h3>
-                  <img src={el?.image} alt="" />
-                  <div className="item-info">
+                  <img src={el?.image} alt='' />
+                  <div className='item-info'>
                     <p>{el?.price}</p>
                     <p>{el?.size}</p>
                   </div>
                   <button
-                    onClick={(e) => {
-                      const basket = JSON.parse(localStorage.getItem("basket"));
-                      const removedItem = basket.map(
-                        (item) =>
-                          item.image === el.image &&
-                          e.target.parentElement.remove()
-                      );
-                      setBasketState(removedItem);
+                    onClick={(e, index) => {
+                      const removeItem = basket.splice(index, i)
+                      setBasketState(basketState.splice(index, i))
+                      localStorage.setItem('basket', JSON.stringify(removeItem))
                     }}
                   >
                     Remove
@@ -45,16 +39,15 @@ const Basket = ({ basketState, setBasketState }) => {
           )}
         </div>
       </div>
-      <div className="checkout">
+      <div className='checkout'>
         <h2>Proceed to checkout</h2>
         <p>Items: </p>
-        <div className="checkout-items">
+        <div className='checkout-items'>
           {basketState?.map(
             (el, i) =>
-              el?.image !==
-                require("../Popups/TradeItem/trade-images/sold.jpg") && (
-                <div ref={checkoutItemRef} key={i} className="checkout-item">
-                  <img src={el?.image} alt="" />
+              el?.image !== require('../Popups/TradeItem/trade-images/sold.jpg') && (
+                <div ref={checkoutItemRef} key={i} className='checkout-item'>
+                  <img src={el?.image} alt='' />
                   <p>{el?.size}</p>
                   <p>{el?.price}</p>
                 </div>
@@ -63,7 +56,7 @@ const Basket = ({ basketState, setBasketState }) => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Basket;
+export default Basket
--- src/components/Popups/TradeItem/TradeItem.jsx
+++ src/components/Popups/TradeItem/TradeItem.jsx
@@ -1,78 +1,63 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useRef, useState, useEffect } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, A11y } from "swiper";
-import { AiOutlineClose } from "react-icons/ai";
-import "./TradeItem.css";
-import "swiper/css";
-import "swiper/css/navigation";
-import { trade } from "./trade";
+import React, { useRef, useState, useEffect } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, A11y } from 'swiper'
+import { AiOutlineClose } from 'react-icons/ai'
+import './TradeItem.css'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { trade } from './trade'
 
-const TradeItem = ({
-  setTrade,
-  gemState,
-  basketState,
-  setBasketState,
-  basket,
-}) => {
-  const [shapeState, setShapeState] = useState("oval");
-  const currentTitle = useRef();
-  const currentImage = useRef();
-  const currentPrice = useRef();
-  const currentSize = useRef();
-  let check = new Set();
+const TradeItem = ({ setTrade, gemState, basketState, setBasketState }) => {
+  const [shapeState, setShapeState] = useState('oval')
+  const currentTitle = useRef()
+  const currentImage = useRef()
+  const currentPrice = useRef()
+  const currentSize = useRef()
+  let check = new Set()
+
   useEffect(() => {
-    localStorage.setItem("basket", JSON.stringify(basketState));
-  }, [basketState]);
+    localStorage.setItem('basket', JSON.stringify(basketState))
+  }, [basketState])
 
   const changeShape = (e) => {
-    setShapeState(e.target.textContent.toLowerCase());
-  };
+    setShapeState(e.target.textContent.toLowerCase())
+  }
 
   const removedDuplicates = trade[0][gemState]
-    ?.filter((gem) => !check.has(gem["shape"]) && check.add(gem["shape"]))
-    ?.map((image) => image.image === require("./trade-images/sold.jpg"));
+    ?.filter((gem) => !check.has(gem['shape']) && check.add(gem['shape']))
+    ?.map((image) => image.image === require('./trade-images/sold.jpg'))
 
   const shapes = trade[0].shapes.map((shape, index) => {
     return (
       <li
-        className={
-          removedDuplicates
-            ? removedDuplicates[index]
-              ? "sold"
-              : "not-sold"
-            : "sold"
-        }
+        className={removedDuplicates ? (removedDuplicates[index] ? 'sold' : 'not-sold') : 'sold'}
         key={index}
         onClick={(e) => changeShape(e)}
       >
         {shape}
       </li>
-    );
-  });
-  const sizes = trade[0].sizes.map((size, index) => (
-    <li key={index}>{size}</li>
-  ));
+    )
+  })
+  const sizes = trade[0].sizes.map((size, index) => <li key={index}>{size}</li>)
 
   const tradeItems = trade[0][gemState]?.map(
-    ({ image, size = "-", price = "-", shape }, index) =>
+    ({ image, size = '-', price = '-', shape }, index) =>
       shape.toLowerCase() === shapeState && (
         <SwiperSlide key={index}>
-          <img src={image} ref={currentImage} alt="" />
-          <div className="trade-price">
+          <img src={image} ref={currentImage} alt='' />
+          <div className='trade-price'>
             <h2 ref={currentSize}>{size}</h2>
             <h2 ref={currentPrice}>{price}</h2>
           </div>
         </SwiperSlide>
       )
-  );
+  )
   return (
-    <div className="trade-container">
-      <div className="trade">
-        <div className="trade-image">
-          <h2 ref={currentTitle}>
-            {gemState[0].toUpperCase() + gemState.substring(1)}
-          </h2>
+    <div className='trade-container'>
+      <div className='trade'>
+        <div className='trade-image'>
+          <h2 ref={currentTitle}>{gemState[0].toUpperCase() + gemState.substring(1)}</h2>
           <Swiper
             modules={[Navigation, Pagination, A11y]}
             spaceBetween={50}
@@ -83,8 +68,8 @@ const TradeItem = ({
               tradeItems
             ) : (
               <SwiperSlide>
-                <img src={require("./trade-images/sold.jpg")} alt="" />
-                <div className="trade-price">
+                <img src={require('./trade-images/sold.jpg')} alt='' />
+                <div className='trade-price'>
                   <h2>-</h2>
                   <h2>-</h2>
                 </div>
@@ -92,18 +77,18 @@ const TradeItem = ({
             )}
           </Swiper>
 
-          <hr className="the-SEPARATOR" />
+          <hr className='the-SEPARATOR' />
         </div>
 
-        <div className="trade-buttons shape">
+        <div className='trade-buttons shape'>
           <ul>{shapes}</ul>
         </div>
 
-        <div className="trade-buttons size">
+        <div className='trade-buttons size'>
           <ul>{sizes}</ul>
         </div>
 
-        <div className="trade-cart">
+        <div className='trade-cart'>
           <button
             onClick={() => {
               setBasketState([
@@ -114,7 +99,7 @@ const TradeItem = ({
                   price: currentPrice.current.innerText,
                   size: currentSize.current.innerText,
                 },
-              ]);
+              ])
             }}
           >
             Add To Cart
@@ -122,14 +107,14 @@ const TradeItem = ({
           <button>Buy Now</button>
         </div>
         <AiOutlineClose
-          className="x"
+          className='x'
           onClick={() => {
-            setTrade(false);
+            setTrade(false)
           }}
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default TradeItem;
+export default TradeItem
