65a2e23f02ab2aa5d73d964522b51a1897362c0f
Ardonit <73855297+ArdonitSaliji@users.noreply.github.com>
2022-08-14 02:20:47 +0200

    rearranging basket elements

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,27 +1,31 @@
-import './Basket.css'
+import "./Basket.css";
 const Basket = ({ basketState }) => {
   return (
-    <div className='basket'>
-      <h2>Basket Items</h2>
-      <div className='basket-items'>
-        {basketState &&
-          basketState.map((el, i) => (
-            <div key={i} className='basket-item'>
-              <h3>{el.title}</h3>
-              <img src={el.image} alt='' />
-              <div className='item-info'>
-                <p>{el.price}</p>
-                <p>{el.size}</p>
+    <div className="basket-container">
+      <div className="basket">
+        <h2>Basket Items</h2>
+        <div className="basket-items">
+          {basketState &&
+            basketState.map((el, i) => (
+              <div key={i} className="basket-item">
+                <h3>{el.title}</h3>
+                <img src={el.image} alt="" />
+                <div className="item-info">
+                  <p>{el.price}</p>
+                  <p>{el.size}</p>
+                </div>
+                <button onClick={(e) => e.target.parentElement.remove()}>
+                  Remove
+                </button>
               </div>
-              <button onClick={(e) => e.target.parentElement.remove()}>Remove</button>
-            </div>
-          ))}
+            ))}
+        </div>
       </div>
-      <div className='basket-checkout'>
-        <h1>h</h1>
+      <div className="checkout">
+        <h2>Proceed to checkout</h2>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Basket
+export default Basket;
