dda831189ee41182f780d0eab11eaccc44fd73b4
ArdonitSaliji <ardonit.1980@gmail.com>
2022-12-24 02:24:18 +0100

    show basket items only if user is logged in

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -13,53 +13,57 @@ const Basket = () => {
     <div className='home'>
       <div className='productContainer'>
         <ListGroup>
-          {basketProducts.map((prod, index) => (
-            <ListGroup.Item key={index}>
-              <Row>
-                <Col md={2}>
-                  <Image src={prod.image} alt={prod.name} fluid rounded />
-                </Col>
-                <Col md={2}>
-                  <span>{prod.name}</span>
-                </Col>
-                <Col md={2}>{prod.price}</Col>
+          {sessionStorage.getItem('isLoggedIn') ? (
+            basketProducts.map((prod, index) => (
+              <ListGroup.Item key={index}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={prod.image} alt={prod.name} fluid rounded />
+                  </Col>
+                  <Col md={2}>
+                    <span>{prod.name}</span>
+                  </Col>
+                  <Col md={2}>{prod.price}</Col>
 
-                <Col md={2}>
-                  <Form.Control
-                    as='select'
-                    value={prod.qty}
-                    // onChange={(e) =>
-                    //   dispatch({
-                    //     type: 'CHANGE_CART_QTY',
-                    //     payload: {
-                    //       id: prod.id,
-                    //       qty: e.target.value,
-                    //     },
-                    //   })
-                    // }
-                  >
-                    {[...Array(prod.inStock).keys()].map((x, index) => (
-                      <option key={index}>{x + 1}</option>
-                    ))}
-                  </Form.Control>
-                </Col>
-                <Col md={2}>
-                  <Button
-                    type='button'
-                    variant='light'
-                    // onClick={() =>
-                    //   dispatch({
-                    //     type: 'REMOVE_FROM_CART',
-                    //     payload: prod,
-                    //   })
-                    // }
-                  >
-                    <AiFillDelete fontSize='20px' />
-                  </Button>
-                </Col>
-              </Row>
-            </ListGroup.Item>
-          ))}
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={prod.qty}
+                      // onChange={(e) =>
+                      //   dispatch({
+                      //     type: 'CHANGE_CART_QTY',
+                      //     payload: {
+                      //       id: prod.id,
+                      //       qty: e.target.value,
+                      //     },
+                      //   })
+                      // }
+                    >
+                      {[...Array(prod.inStock).keys()].map((x, index) => (
+                        <option key={index}>{x + 1}</option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      // onClick={() =>
+                      //   dispatch({
+                      //     type: 'REMOVE_FROM_CART',
+                      //     payload: prod,
+                      //   })
+                      // }
+                    >
+                      <AiFillDelete fontSize='20px' />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))
+          ) : (
+            <h3>You need to login to view your basket products.</h3>
+          )}
         </ListGroup>
       </div>
       <div className='filters summary'>
