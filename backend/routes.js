7e3ec9da34ff5e4de36d806ed4514fea7e269808
proG <ardonit@ardonit.192.168.1.12>
2022-12-29 00:09:00 +0100

    Made login dropdown mobile responsive

--- backend/routes.js
+++ backend/routes.js
@@ -1,29 +1,29 @@
-const stripe = require('stripe');
-const express = require('express');
+const stripe = require("stripe");
+const express = require("express");
 const router = express.Router();
-const Users = require('./Schema/Users');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const Products = require('./Schema/Products');
+const Users = require("./Schema/Users");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Products = require("./Schema/Products");
 let stripeGateway = stripe(
-  'sk_test_51MCkXgDplZI5a2XjhslbZ4ge0UxGgEIAOvgGnpRRwta6scGnPvWFBlvaYhYqAXvnHK58tHwnUw133AZOpIma1H4q005lxIADbl'
+  "sk_test_51MCkXgDplZI5a2XjhslbZ4ge0UxGgEIAOvgGnpRRwta6scGnPvWFBlvaYhYqAXvnHK58tHwnUw133AZOpIma1H4q005lxIADbl"
 );
-let DOMAIN = 'http://localhost:3000/';
-router.post('/user/checkout', async (req, res) => {
+let DOMAIN = "http://localhost:3000/";
+router.post("/user/checkout", async (req, res) => {
   const session = await stripeGateway.checkout.sessions.create({
-    payment_method_types: ['card'],
-    mode: 'payment',
+    payment_method_types: ["card"],
+    mode: "payment",
     success_url: DOMAIN,
     cancel_url: `${DOMAIN}basket`,
     line_items: req.body.items.map((item) => {
       return {
         price_data: {
-          currency: 'usd',
+          currency: "usd",
           product_data: {
             name: item.name.slice(0, -1),
             description: item.size,
           },
-          unit_amount: Number(item.price.split('$').join('')) * 100,
+          unit_amount: Number(item.price.split("$").join("")) * 100,
         },
         quantity: 1,
       };
@@ -32,32 +32,38 @@ router.post('/user/checkout', async (req, res) => {
   res.status(200).json(session.url);
 });
 
-router.get('/api/products/find', async (req, res) => {
+router.get("/api/products/find", async (req, res) => {
   let foundProduct = await Products.find({
-    $expr: { $gt: [{ $strLenCP: '$text' }, 1] },
+    $expr: { $gt: [{ $strLenCP: "$text" }, 1] },
   });
   foundProduct
     ? res.status(202).json(foundProduct)
-    : res.status(204).send({ message: 'No Products Available' });
+    : res.status(204).send({ message: "No Products Available" });
 });
 
-router.post('/user/cart/add', async (req, res) => {
+router.post("/user/cart/add", async (req, res) => {
   let foundProduct = await Products.findOne({ name: req.body.product });
   let foundUser = await Users.findOne({
     username: req.body.user,
-    'cart.product': foundProduct.name,
+    "cart.product": foundProduct.name,
   });
   if (foundUser) {
-    return res.status(203).json({ message: 'Product already exists' });
+    return res.status(203).json({ message: "Product already exists" });
   } else {
     await Users.findOne({ username: req.body.user }).updateOne({
-      $push: { cart: { _id: foundProduct._id, product: foundProduct.name, quantity: 1 } },
+      $push: {
+        cart: {
+          _id: foundProduct._id,
+          product: foundProduct.name,
+          quantity: 1,
+        },
+      },
     });
     return res.status(202).send(foundProduct);
   }
 });
 
-router.post('/user/cart/delete', async (req, res) => {
+router.post("/user/cart/delete", async (req, res) => {
   const user = await Users.updateOne(
     { username: req.body.user },
     {
@@ -80,25 +86,29 @@ router.post('/user/cart/delete', async (req, res) => {
   }
 });
 
-router.post('/user/cart/products', async (req, res) => {
+router.post("/user/cart/products", async (req, res) => {
   const foundUser = await Users.findOne({
     username: req.body.user,
   });
   if (foundUser) {
-    const basketProducts = await Products.find({ _id: { $in: foundUser.cart } });
+    const basketProducts = await Products.find({
+      _id: { $in: foundUser.cart },
+    });
 
     return res.status(200).send({ basketProducts, foundUser });
   }
 });
 
-router.post('/api/products/select', async (req, res) => {
-  let foundProduct = await Products.find({ name: { $regex: `^${req.body.name}` } });
+router.post("/api/products/select", async (req, res) => {
+  let foundProduct = await Products.find({
+    name: { $regex: `^${req.body.name}` },
+  });
   foundProduct
     ? res.status(202).json(foundProduct)
-    : res.status(204).send({ message: 'No Products Available' });
+    : res.status(204).send({ message: "No Products Available" });
 });
 
-router.get('/api/products', async (req, res) => {
+router.get("/api/products", async (req, res) => {
   let allProducts = await Products.find({});
   if (allProducts) {
     res.status(200).json(allProducts);
@@ -107,20 +117,20 @@ router.get('/api/products', async (req, res) => {
   }
 });
 
-router.post('/api/products/find/shape', async (req, res) => {
+router.post("/api/products/find/shape", async (req, res) => {
   let foundProductShapes = await Products.find({ name: req.body.name });
   res.status(202).json(foundProductShapes);
 });
 
 const authenticateToken = async (req, res, next) => {
-  const token = req.headers['x-access-token'];
+  const token = req.headers["x-access-token"];
 
   if (!token) {
-    res.status(401).send({ msg: 'No token provided!' });
+    res.status(401).send({ msg: "No token provided!" });
   } else {
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
       if (err) {
-        res.json({ auth: false, message: 'Authentication failed' });
+        res.json({ auth: false, message: "Authentication failed" });
       } else {
         req.userId = decoded.id;
         next();
@@ -129,11 +139,11 @@ const authenticateToken = async (req, res, next) => {
   }
 };
 
-router.get('/api/auth', authenticateToken, async (req, res) => {
-  res.send({ message: 'Authenticated Succesfully' });
+router.get("/api/auth", authenticateToken, async (req, res) => {
+  res.send({ message: "Authenticated Succesfully" });
 });
 
-router.post('/api/login', async (req, res) => {
+router.post("/api/login", async (req, res) => {
   const userLoggingIn = req.body;
   const emailOrUsername = req.body.email;
 
@@ -148,34 +158,40 @@ router.post('/api/login', async (req, res) => {
   else foundUser = userExists;
 
   if (!foundUser) {
-    return res.status(404).send({ error: 'Account does not exist' });
+    return res.status(404).send({ error: "Account does not exist" });
   }
   if (foundUser) {
-    const basketProducts = await Products.find({ _id: { $in: foundUser.cart } });
+    const basketProducts = await Products.find({
+      _id: { $in: foundUser.cart },
+    });
     bcrypt.compare(userLoggingIn.password, foundUser.password, (err, user) => {
       if (user) {
         const signUser = { username: emailOrUsername };
-        const accessToken = jwt.sign(signUser, process.env.ACCESS_TOKEN_SECRET, {
-          expiresIn: '1d',
-        });
+        const accessToken = jwt.sign(
+          signUser,
+          process.env.ACCESS_TOKEN_SECRET,
+          {
+            expiresIn: "1d",
+          }
+        );
 
         req.session.user = user;
         res.status(200).json({
           auth: true,
           accessToken: accessToken,
           username: emailOrUsername,
-          success: 'Login successful',
+          success: "Login successful",
           isLoggedIn: true,
           basketProducts: basketProducts,
         });
       } else {
-        res.status(401).send({ error: 'Incorrect Username/Email or Password' });
+        res.status(401).send({ error: "Incorrect Username/Email or Password" });
       }
     });
   }
 });
 
-router.post('/api/signup', async (req, res) => {
+router.post("/api/signup", async (req, res) => {
   const user = req.body;
   const takenEmail = await Users.findOne({
     email: req.body.email,
@@ -184,9 +200,9 @@ router.post('/api/signup', async (req, res) => {
     username: user.username,
   });
   if (takenEmail) {
-    return res.status(409).json({ error: 'Email already exists!' });
+    return res.status(409).json({ error: "Email already exists!" });
   } else if (takenUsername) {
-    return res.status(409).json({ error: 'Username already exists!' });
+    return res.status(409).json({ error: "Username already exists!" });
   } else {
     if (user.password1 === user.password2) {
       user.password1 = await bcrypt.hash(req.body.password1, 10);
@@ -196,15 +212,15 @@ router.post('/api/signup', async (req, res) => {
         password: user.password1,
       });
       await dbUser.save();
-      return res.status(201).json({ success: 'Account created successfully!' });
+      return res.status(201).json({ success: "Account created successfully!" });
     }
   }
-  res.status(403).json({ error: 'Passwords must be matching!' });
+  res.status(403).json({ error: "Passwords must be matching!" });
 });
 
-router.get('/api/logout', (req, res) => {
+router.get("/api/logout", (req, res) => {
   req.session.destroy();
-  res.clearCookie('connect.sid'); // clean up!
-  return res.status(200).json({ msg: 'logging you out' });
+  res.clearCookie("connect.sid"); // clean up!
+  return res.status(200).json({ msg: "logging you out" });
 });
 module.exports = router;
--- src/components/Navbar/Login.jsx
+++ src/components/Navbar/Login.jsx
@@ -1,53 +1,63 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { updateBasket } from '../../feature/basketSlice';
-import { toast } from 'react-toastify';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { updateBasket } from "../../feature/basketSlice";
+import { toast } from "react-toastify";
+import { useMediaQuery } from "@chakra-ui/react";
 const Login = ({ login, setIsLoggedIn }) => {
-  const [loginInUser, setLoginInUser] = useState({ email: '', password: '' });
+  const [loginInUser, setLoginInUser] = useState({ email: "", password: "" });
   const [message, setMessage] = useState();
   const dispatch = useDispatch();
   const loginUser = async () => {
-    const res = await fetch('http://localhost:5000/api/login', {
-      method: 'POST',
+    const res = await fetch("http://localhost:5000/api/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email: loginInUser.email, password: loginInUser.password }),
+      body: JSON.stringify({
+        email: loginInUser.email,
+        password: loginInUser.password,
+      }),
     });
     const json = await res.json();
     if (res.status === 200) {
-      toast.success('Login successful!', {
-        position: 'top-center',
+      toast.success("Login successful!", {
+        position: "top-center",
       });
-      sessionStorage.setItem('isLoggedIn', JSON.stringify(json.isLoggedIn));
-      sessionStorage.setItem('user', JSON.stringify(json.username));
+      sessionStorage.setItem("isLoggedIn", JSON.stringify(json.isLoggedIn));
+      sessionStorage.setItem("user", JSON.stringify(json.username));
       console.log(json.basketProducts);
       dispatch(updateBasket(json.basketProducts));
       setIsLoggedIn(json.isLoggedIn);
       setMessage(json.success);
-      document.querySelector('.login-message').classList.add('show', 'success');
+      document.querySelector(".login-message").classList.add("show", "success");
     } else {
-      document.querySelector('.login-message').classList.add('show', 'error');
+      document.querySelector(".login-message").classList.add("show", "error");
       setMessage(json.error);
       setTimeout(() => {
-        setMessage('');
+        setMessage("");
       }, 6000);
     }
   };
-
+  const media = useMediaQuery("(max-width: 771px)")[0];
   return (
     <div
-      className={login ? 'navbar-tab navbar-tab-login show-login' : 'navbar-tab navbar-tab-login'}
+      className={
+        media
+          ? login
+            ? "navbar-tab navbar-tab-login mobile show-login"
+            : "navbar-tab navbar-tab-login mobile"
+          : !login
+          ? "navbar-tab navbar-tab-login mobile show-login"
+          : "navbar-tab navbar-tab-login mobile"
+      }
     >
-      <div className='left'>
-        <form name='login' className='login-form'>
-          <h4 className='login-title'>Login</h4>
-          <br />
-          <br />
-          <div className='login-inputs'>
-            <div className='form-group'>
-              <label required htmlFor='email' className='sr-only'>
+      <div className="left">
+        <form name="login" className="login-form">
+          <h4 className="login-title">Login</h4>
+          <div className="login-inputs">
+            <div className="form-group">
+              <label required htmlFor="email" className="sr-only">
                 Username or Email
               </label>
               <input
@@ -55,52 +65,59 @@ const Login = ({ login, setIsLoggedIn }) => {
                   setLoginInUser({ ...loginInUser, email: e.target.value });
                 }}
                 required={true}
-                type='text'
-                placeholder='Username or email'
-                name='email'
-                id='email'
-                className='form-control'
+                type="text"
+                placeholder="Username or email"
+                name="email"
+                id="email"
+                className="form-control"
               />
             </div>
-            <div className='form-group'>
-              <label required htmlFor='password' className='sr-only'>
+            <div className="form-group">
+              <label required htmlFor="password" className="sr-only">
                 Password
               </label>
               <input
                 onChange={(e) => {
                   setLoginInUser({ ...loginInUser, password: e.target.value });
                 }}
-                id='password'
-                type='password'
-                placeholder='Password'
+                id="password"
+                type="password"
+                placeholder="Password"
                 required
-                name='password'
-                className='form-control'
+                name="password"
+                className="form-control"
               />
             </div>
           </div>
           <Link
             onClick={() =>
-              document.querySelector('.navbar-tab navbar-tab-login').classList.remove('show-login')
+              document
+                .querySelector(".navbar-tab navbar-tab-login")
+                .classList.remove("show-login")
             }
-            to={'/reset-password'}
+            to={"/reset-password"}
           >
             Forgot password?
           </Link>
         </form>
       </div>
-      <span className='login-message'>{message}</span>
-      <div className='right'>
+      <span className="login-message">{message}</span>
+      <div className="right">
         <form>
           <button
-            type='button'
-            className='btn btn-primary'
+            type="button"
+            className="btn btn-primary"
             onClick={() => {
-              if (loginInUser.email.length < 1 && loginInUser.password.length < 1) {
-                setMessage('← Fields are empty!');
-                document.querySelector('.login-message').classList.add('show', 'error');
+              if (
+                loginInUser.email.length < 1 &&
+                loginInUser.password.length < 1
+              ) {
+                setMessage("← Fields are empty!");
+                document
+                  .querySelector(".login-message")
+                  .classList.add("show", "error");
                 setTimeout(() => {
-                  setMessage('');
+                  setMessage("");
                 }, 5000);
               } else {
                 loginUser();
@@ -109,8 +126,7 @@ const Login = ({ login, setIsLoggedIn }) => {
           >
             Login
           </button>
-          <hr />
-          <button type='button' className='btn btn-accent1'>
+          <button type="button" className="btn btn-accent1">
             Create new account
           </button>
         </form>
