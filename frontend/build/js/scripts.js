6f7795e3fe35a04c49d03c87ec85260c4708247f
Ardonit Saliji <ardonit.1980@gmail.com>
2023-01-05 02:34:02 +0100

    build

---
+++ frontend/build/js/scripts.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   window.addEventListener('scroll', () => {
+//     let navbarHeight = document.querySelector('.navbar-container').scrollHeight;
+//     let windowPosition = window.scrollY;
+//     if (windowPosition > navbarHeight - 50) {
+//       document.querySelector('.navbar-container').classList.add('sticky');
+//     } else {
+//       document.querySelector('.navbar-container').classList.remove('sticky');
+//     }
+//   });
+// });
